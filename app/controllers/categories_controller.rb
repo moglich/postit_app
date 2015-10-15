d70076ee6653e05d0875f34baff56e53d8d91994
Remo Regli <remo.regli@gmail.com>
2015-10-15 10:21:34 +0200

    added category view

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+#  def new
+#    @post = Post.new
+#  end
+#
+#  def create
+#    @post = Post.new(post_params) 
+#    @post.creator = User.first
+#
+#    if @post.save
+#      flash[:notice] = "Your post was created."
+#      redirect_to posts_path
+#    else
+#      render 'new'
+#    end
+#  end
+#
+#  def edit
+#  end
+#
+#  def update
+#    if @post.update(post_params)
+#      flash[:notice] = "Your post was updated."
+#      redirect_to post_path(@post)
+#    else
+#      render 'edit'
+#    end
+#  end
+#
+#  private
+#
+#  def post_params
+#    params.require(:post).permit!
+#  end
+#
+#  def set_post
+#    @post = Post.find(params[:id])
+#  end
+    
+end
