db0814774751f624781005e58f0b3dbfe57fd6b3
Remo Regli <remo.regli@gmail.com>
2016-01-03 11:02:24 +0100

    added slugs to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120120544) do
+ActiveRecord::Schema.define(version: 20160103083910) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151120120544) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   create_table "users", force: true do |t|
