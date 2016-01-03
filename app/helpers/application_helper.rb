686a8bf0ccbee5f13fc379f9c0622ca21230d82d
Remo Regli <remo.regli@gmail.com>
2016-01-03 12:30:26 +0100

    added time zones for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,9 @@ module ApplicationHelper
   end
 
   def display_datetime(dt)
+    if logged_in? and !current_user.time_zone.blank?
+      dt = dt.in_time_zone(current_user.time_zone)
+    end
     dt.strftime("%d.%m.%Y %H:%M %Z")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103100551) do
+ActiveRecord::Schema.define(version: 20160103110238) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160103100551) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "role"
+    t.string   "time_zone"
   end
 
   create_table "votes", force: true do |t|
